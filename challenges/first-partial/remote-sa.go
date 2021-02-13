9cbef8009063c035298d0bb1caedd349e503ca4e
Homar Cano <homarcanojr@hotmail.com>
2021-02-13 23:49:59 +0000

    First partial finished
    Signed-off-by: Homar Cano <homarcanojr@hotmail.com>

--- challenges/first-partial/remote-sa.go
+++ challenges/first-partial/remote-sa.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"strconv"
 	"strings"
@@ -45,14 +46,42 @@ func generatePoints(s string) ([]Point, error) {
 
 // getArea gets the area inside from a given shape
 func getArea(points []Point) float64 {
-	// Your code goes here
-	return 0.0
+	area := 0.0
+	for i := 0; i < len(points); i++ {
+		x1 := points[i].X
+		y1 := points[i].Y
+		x2 := 0.0
+		y2 := 0.0
+		if i == len(points)-1 {
+			x2 = points[0].X
+			y2 = points[0].Y
+		} else {
+			x2 = points[i+1].X
+			y2 = points[i+1].Y
+		}
+		area += ((y1 + y2) / 2) * (x2 - x1)
+	}
+	return area
 }
 
 // getPerimeter gets the perimeter from a given array of connected points
 func getPerimeter(points []Point) float64 {
-	// Your code goes here
-	return 0.0
+	perimeter := 0.0
+	for i := 0; i < len(points); i++ {
+		x1 := points[i].X
+		y1 := points[i].Y
+		x2 := 0.0
+		y2 := 0.0
+		if i == len(points)-1 {
+			x2 = points[0].X
+			y2 = points[0].Y
+		} else {
+			x2 = points[i+1].X
+			y2 = points[i+1].Y
+		}
+		perimeter += math.Sqrt(math.Pow((x2-x1), 2) + math.Pow((y2-y1), 2))
+	}
+	return perimeter
 }
 
 // handler handles the web request and reponds it
@@ -81,9 +110,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// Response construction
 	response := fmt.Sprintf("Welcome to the Remote Shapes Analyzer\n")
 	response += fmt.Sprintf(" - Your figure has : [%v] vertices\n", len(vertices))
+	if len(vertices) < 3 {
+		response += "ERROR - Your shape is not compliying with the minimum number of vertices.\n"
+		fmt.Fprintf(w, response)
+		return
+	}
 	response += fmt.Sprintf(" - Vertices        : %v\n", vertices)
-	response += fmt.Sprintf(" - Perimeter       : %v\n", perimeter)
-	response += fmt.Sprintf(" - Area            : %v\n", area)
+	response += fmt.Sprintf(" - Perimeter       : %.1f\n", perimeter)
+	response += fmt.Sprintf(" - Area            : %.1f\n", area)
 
 	// Send response to client
 	fmt.Fprintf(w, response)
